f39f2a45e22b79c14f08909bcf185b3b7691bd3b
Bo Frederiksen <bofrede@bofrede.dk>
2013-04-30 22:34:38 +0200

    Moved javascript to the bottom of the page, and into external files.

---
+++ demo.js
@@ -0,0 +1,14 @@
+/*jslint browser: true, sloppy: true */
+/*global Tabs: false */
+(function () {
+	var links = document.getElementById("gallery-preview").getElementsByTagName("a"),
+	    loading = new Image();
+	loading.src = "images/loading.png";
+	Tabs.create(links, {
+		show: function () {
+			Tabs.getTarget(this).src = loading.src;
+			Tabs.getTarget(this).src = this.href.replace(/#[\w\-]+$/, "");
+			return false;
+		}
+	});
+}());
