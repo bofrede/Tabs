5bf2bb09be6ff4dba7c33edf1a043c7ba05d8966
Bo Frederiksen <bo.frederiksen@semaphor.dk>
2022-04-13 17:01:45 +0200

    Simplified example.

--- tabs.js
+++ tabs.js
@@ -1,4 +1,6 @@
 /*jslint browser: true, plusplus: true, sloppy: true */
+/*global document: false */
+/* jshint -W100 */
 /*
 *    TABS
 *    ---------------------------------------------------------------------------
@@ -7,9 +9,6 @@
 *
 *    Copyright (c) 2008 Dan Peverill
 *    http://www.danpeverill.com
-*
-*    Copyright (c) 2005 John Resig
-*    http://ejohn.org/projects/flexible-javascript-events/
 *
 *    LICENSE
 *    ---------------------------------------------------------------------------
@@ -40,24 +39,12 @@
 *    two arguments, the click event and the currently active tab target. "this" refers to the tab.
 *    click: This callback is triggered just as a tab is clicked. Returning false cancels the entire event.
 *    show: This callback is triggered after the active class and tab has been set, but just before the
-*        tab targets are shown. Returning false means you handled the showing/hiding of the tab targets.
+*    tab targets are shown. Returning false means you handled the showing/hiding of the tab targets.
 */
 
 var Tabs = {
     className: "tabs",
     activeClass: "active",
-
-    addEvent: function (obj, type, fn) {
-        if (obj.attachEvent) {
-            obj['e' + type + fn] = fn;
-            obj[type + fn] = function () {
-                obj['e' + type + fn](window.event);
-            };
-            obj.attachEvent('on' + type, obj[type + fn]);
-        } else {
-            obj.addEventListener(type, fn, false);
-        }
-    },
 
     create: function (tabs, callbacks) {
         if (!tabs.length) {
@@ -68,21 +55,18 @@ var Tabs = {
     },
 
     createSingle: function (tab, callbacks) {
-        if (this.Element.hasClass(tab, this.activeClass)) {
-            this.Element.show(this.getTarget(tab));
+        if (tab.classList.contains(this.activeClass)) {
+            this.getTarget(tab).style.display = "";
         }
-        this.addEvent(tab, "click", function (e) {
+        tab.addEventListener("click", function (e) {
             if (Tabs.callback(this, callbacks, "click", e)) {
-                Tabs.Element.toggleClass(this, Tabs.activeClass);
+                e.target.classList.toggle(Tabs.activeClass);
                 if (Tabs.callback(this, callbacks, "show", e)) {
-                    Tabs.Element.toggleVisibility(Tabs.getTarget(this));
+                    var target = Tabs.getTarget(this);
+                    target.style.display = (target.style.display !== "none") ? "none" : "";
                 }
             }
-            if (e.preventDefault) { // For DOM compliant browsers http://www.w3.org/TR/2000/REC-DOM-Level-2-Events-20001113/events.html#Events-Event-preventDefault
-                e.preventDefault();
-            } else { // For MSIE http://msdn2.microsoft.com/en-us/library/ms536913.aspx
-                e.returnValue = false;
-            }
+            e.preventDefault();
         });
     },
 
@@ -92,37 +76,33 @@ var Tabs = {
             i;
         function tabClickHandler(e) {
             if (Tabs.callback(this, callbacks, "click", e, active)) {
-                Tabs.Element.removeClass(active, Tabs.activeClass);
-                Tabs.Element.addClass(this, Tabs.activeClass);
+                active.classList.remove(Tabs.activeClass);
+                e.target.classList.add(Tabs.activeClass);
                 var from = active;
                 active = this;
                 if (Tabs.callback(this, callbacks, "show", e, from)) {
-                    Tabs.Element.hide(Tabs.getTarget(from));
-                    Tabs.Element.show(Tabs.getTarget(this));
+                    Tabs.getTarget(from).style.display = "none";
+                    Tabs.getTarget(this).style.display = "";
                 }
             }
-            if (e.preventDefault) { // For DOM compliant browsers http://www.w3.org/TR/2000/REC-DOM-Level-2-Events-20001113/events.html#Events-Event-preventDefault
-                e.preventDefault();
-            } else { // For MSIE http://msdn2.microsoft.com/en-us/library/ms536913.aspx
-                e.returnValue = false;
-            }
+            //e.preventDefault();
         }
         for (i = 0; i < tabs.length; i++) {
             tab = tabs[i];
-            if (this.Element.hasClass(tab, this.activeClass)) {
+            if (tab.classList.contains(this.activeClass)) {
                 active = tab;
-                this.Element.addClass(tab);
-                this.Element.show(this.getTarget(tab));
+                tab.classList.add(Tabs.activeClass);
+                this.getTarget(tab).style.display = "";
             } else {
-                this.Element.hide(this.getTarget(tab));
+                this.getTarget(tab).style.display = "none";
             }
-            this.addEvent(tab, "click", tabClickHandler);
+            tab.addEventListener("click", tabClickHandler);
         }
         if (!active) {
             tab = tabs[0];
             active = tab;
-            this.Element.addClass(tab, this.activeClass);
-            this.Element.show(this.getTarget(tab));
+            tab.classList.add(this.activeClass);
+            this.getTarget(tab).style.display = "";
         }
     },
 
@@ -131,108 +111,33 @@ var Tabs = {
     },
 
     getTarget: function (tab) {
-        var match = /#([\w-]+)$/.exec(tab.href),
-            target;
-        if (match && (target == document.getElementById(match[1]))) {
-            return target;
-        }
-    },
-
-    getElementsByClassName: function (className, tag) {
-        var elements = document.getElementsByTagName(tag || "*"),
-            list = [],
-            i;
-        for (i = 0; i < elements.length; i++) {
-            if (this.Element.hasClass(elements[i], this.className)) {
-                list.push(elements[i]);
-            }
-        }
-        return list;
-    }
-};
-
-Tabs.Element = {
-    addClass: function (element, className) {
-        element.className += (element.className ? " " : "") + className;
-    },
-
-    removeClass: function (element, className) {
-        element.className = element.className.replace(new RegExp("(^|\\s)" + className + "(\\s|$)"), "$1");
-        if (element.className === " ") {
-            element.className = "";
-        }
-    },
-
-    hasClass: function (element, className) {
-        return element.className && (new RegExp("(^|\\s)" + className + "(\\s|$)")).test(element.className);
-    },
-
-    toggleClass: function (element, className) {
-        if (this.hasClass(element, className)) {
-            this.removeClass(element, className);
-        } else {
-            this.addClass(element, className);
-        }
-    },
-
-    getStyle: function (element, property) {
-        if (element.style[property]) {
-            return element.style[property];
-        }
-        if (element.currentStyle) {    // IE.
-            return element.currentStyle[property];
-        }
-        property = property.replace(/([A-Z])/g, "-$1").toLowerCase();    // Turns propertyName into property-name.
-        var style = document.defaultView.getComputedStyle(element, "");
-        if (style) {
-            return style.getPropertyValue(property);
-        }
-    },
-
-    show: function (element) {
-        element.style.display = "";
-        if (this.getStyle(element, "display") === "none") {
-            element.style.display = "block";
-        }
-    },
-
-    hide: function (element) {
-        element.style.display = "none";
-    },
-
-    isVisible: function (element) {
-        return this.getStyle(element, "display") !== "none";
-    },
-
-    toggleVisibility: function (element) {
-        if (this.isVisible(element)) {
-            this.hide(element);
-        } else {
-            this.show(element);
+        var match = /#([\w-]+)$/.exec(tab.href);
+        if (match) {
+            return document.getElementById(match[1]);
         }
     }
 };
 
 (function () {
-    var elements = Tabs.getElementsByClassName(Tabs.className),
+    var tabSetList = document.getElementsByClassName(Tabs.className),
         i,
-        element,
+        tabSet,
         tabs,
         group,
         t;
-    for (i = 0; i < elements.length; i++) {
-        element = elements[i];
-        if (element.tagName === "A" || element.tagName === "AREA") {
-            Tabs.create(element);
-        } else {    // Group
-            tabs = element.getElementsByTagName("a");
+    for (i = 0; i < tabSetList.length; i++) {
+        tabSet = tabSetList[i];
+        if (tabSet.tagName === "A" || tabSet.tagName === "AREA") {
+            Tabs.create(tabSet);
+        } else { // Group
+            tabs = tabSet.getElementsByTagName("a");
             if (tabs.length === 0) {
-                tabs = element.getElementsByTagName("area");
+                tabs = tabSet.getElementsByTagName("area");
             }
             group = [];
             for (t = 0; t < tabs.length; t++) {
                 if (Tabs.getTarget(tabs[t])) {
-                    group.push(tabs[t]);    // Only group actual tab links.
+                    group.push(tabs[t]); // Only group actual tab links.
                 }
             }
             if (group.length) {
